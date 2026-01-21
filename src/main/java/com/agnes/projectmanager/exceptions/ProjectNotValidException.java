7339bd529bc1aea01fb85b519ccdc5c675cb77cd
agnesmanoel <agnesmanoel55@gmail.com>
2026-01-20 22:59:10 -0300

    feat: criação de excecoes personalizadas

---
+++ src/main/java/com/agnes/projectmanager/exceptions/ProjectNotValidException.java
@@ -0,0 +1,9 @@
+package com.agnes.projectmanager.exceptions;
+
+public class ProjectNotValidException extends RuntimeException {
+
+    public ProjectNotValidException(String message){
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/agnes/projectmanager/exceptions/UserNotValidException.java
@@ -0,0 +1,9 @@
+package com.agnes.projectmanager.exceptions;
+
+public class UserNotValidException extends RuntimeException{
+
+    public UserNotValidException(String message){
+        super(message);
+    }
+
+}
