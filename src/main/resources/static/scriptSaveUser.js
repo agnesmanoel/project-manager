5477fa15684766cdc64ebfe60aafab1b8ed0fe2b
agnesmanoel <agnesmanoel55@gmail.com>
2026-01-21 16:17:30 -0300

    implementa salvar usuario

---
+++ src/main/resources/static/scriptSaveUser.js
@@ -0,0 +1,28 @@
+
+const userForms = document.querySelector("#formsUser");
+
+
+async function saveUser(event) {
+
+    event.preventDefault(); 
+    const formData = new FormData(userForms);
+
+    const data = Object.fromEntries(formData.entries());
+    const checkbox = document.getElementById("userHasAgreed");
+    data.userHasAgreed = checkbox ? checkbox.checked : false;
+
+    const response = await fetch("http://localhost:8080/users", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+
+        });
+
+        if (!response.ok) {
+            alert(response.text())
+        } 
+
+}
+
+
+userForms.addEventListener("submit", saveUser);
