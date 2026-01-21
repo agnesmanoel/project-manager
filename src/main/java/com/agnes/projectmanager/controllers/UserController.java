8b38acc1ca537ed83853a942d38129b8157693ce
agnesmanoel <agnesmanoel55@gmail.com>
2026-01-21 16:01:54 -0300

    implementa carregamento de usuarios na tela

--- src/main/java/com/agnes/projectmanager/controllers/UserController.java
+++ src/main/java/com/agnes/projectmanager/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.agnes.projectmanager.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,8 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "*")
+
 public class UserController {
 
     @Autowired
---
+++ src/main/resources/static/scriptGetUsers.js
@@ -0,0 +1,38 @@
+
+const tela = document.getElementById("tela");
+
+
+async function getAllUsers() {
+
+    const response = await fetch("http://localhost:8080/users");
+    const users = await response.json();
+
+    users.forEach(user => {
+
+        const div = document.createElement("div");
+
+        div.classList.add("card");
+
+        const userName = document.createElement("p");
+        const userNumber = document.createElement("p");
+        const image = document.createElement("img");
+
+        image.src = "user-profile.png";
+        image.width = 100;
+        image.height = 100;
+
+ 
+
+        userName.innerText = "User name: " + user.userName;
+        userNumber.innerText = "User number: " + user.userNumber;
+        div.append(image, userName, userNumber);
+        tela.appendChild(div);
+
+
+    });
+
+}
+
+ 
+
+getAllUsers()
