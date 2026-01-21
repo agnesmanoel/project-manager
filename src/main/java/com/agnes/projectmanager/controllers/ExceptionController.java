9b15844bf8b4343aa8da1c90eb2e75b3eff4d93a
agnesmanoel <agnesmanoel55@gmail.com>
2026-01-20 23:15:34 -0300

    feat: cria exception controller

---
+++ src/main/java/com/agnes/projectmanager/controllers/ExceptionController.java
@@ -0,0 +1,19 @@
+package com.agnes.projectmanager.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.agnes.projectmanager.exceptions.UserNotValidException;
+
+
+@RestControllerAdvice
+public class ExceptionController {
+    
+    @ExceptionHandler(UserNotValidException.class)
+    public ResponseEntity<String> noNameUser(UserNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+}
